aa6d1e142eefa48872b89a2c10801a020f9ce811
unknown <Ayush-dev-45>
2024-08-07 21:22:43 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import path from "path";
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
+import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
+import { fileURLToPath } from "url";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.resolve();
+
+
+const result = dotenv.config();
+if (result.error) {
+    console.error('Error loading .env file:', result.error);
+}
+//console.log(process.env.PORT);
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); 
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
+server.listen(PORT, () => {
+	connectToMongoDB();
+	console.log(`Server Running on ${PORT}`);
+});
