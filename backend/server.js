55dd159647bb4c6fe29e488775dc1b5d822b6a65
Ayush-dev-45 <ayushneo45@gmail.com>
2024-08-07 21:43:31 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,15 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
-
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 import { fileURLToPath } from "url";
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.resolve();
-
 
 const result = dotenv.config();
 if (result.error) {
